07e0cb161a719d0e6891a150801cc9468062193d
Sokol Paja <sokol.paja@inova.al>
2023-09-25 18:06:44 +0200

    try vvue

---
+++ src/App.vue
@@ -0,0 +1,44 @@
+<script setup>
+import WelcomeItem from './components/WelcomeItem.vue'
+import IconDocumentation from './components/icons/IconDocumentation.vue'
+</script>
+
+<template>
+  <main>
+    <header >
+      <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+    </header>
+
+    <section>
+      <h1>Trying  Vue</h1>
+
+      <WelcomeItem>
+        <template #icon>
+          <IconDocumentation />
+        </template>
+        <template #default>Quick Start Docs</template>
+        <template #heading>
+          Start learning vue
+        </template>
+      </WelcomeItem>
+    </section>
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+}
+main{
+  display: flex;
+  justify-content: center;
+  flex-direction: column;
+  align-items: center;
+  margin: 0 auto;
+  width: 100%;
+}
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+</style>
---
+++ src/components/Wrap.vue
@@ -0,0 +1,12 @@
+<template>
+     <div class="wrap">
+
+     </div>
+</template>
+
+<style scoped>
+.wrap{
+    padding: 25px;
+    border: 1px solid rebeccapurple;
+}
+</style>
